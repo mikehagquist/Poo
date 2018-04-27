42fb7980c1d958613baf0ccddf4fb9bdd0f522ac
EC2 Default User <ec2-user@ip-172-31-28-28.us-east-2.compute.internal>
2018-04-27 17:44:58 +0000

    More work on schema and player list.

--- Database/models.py
+++ Database/models.py
@@ -3,39 +3,86 @@ from django.db import models
 class League(models.Model):
     leaguename = models.TextField()
     active = models.BooleanField(default=True)
+    createdate = models.DateTimeField()
+    lastupdate = models.DateTimeField()
     
     def __str__(self):
         return self.name
 
 class Course(models.Model):
-    League = models.ForeignKey(League, on_delete=models.CASCADE)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
     coursename = models.CharField(max_length=200)
+    coursepar = models.PositiveSmallIntegerField()
+    frontpar = models.PositiveSmallIntegerField()
+    backpar = models.PositiveSmallIntegerField()
     active = models.BooleanField(default=True)
+    createdate = models.DateTimeField()
+    lastupdate = models.DateTimeField()
 
     def __str__(self):
         return self.name
 
 class CourseHoles(models.Model):
-    Course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    League = models.ForeignKey(League, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
     holenumber = models.PositiveSmallIntegerField()
     holepar = models.PositiveSmallIntegerField()
     holehandicapmen = models.PositiveSmallIntegerField()
     holehandicapladies = models.PositiveSmallIntegerField()
+    frontback = (
+        ('F', 'Front Nine'),
+        ('B', 'Back Nine'),
+    )
+    createdate = models.DateTimeField()
+    lastupdate = models.DateTimeField()
 
     def __str__(self):
         return self.name
 
-class Roster(models.Model):
-    League = models.ForeignKey(League, on_delete=models.CASCADE)
-    headline = models.CharField(max_length=255)
-    body_text = models.TextField()
-    pub_date = models.DateField()
-    mod_date = models.DateField()
-    #authors = models.ManyToManyField(Author)
-    n_comments = models.IntegerField()
-    n_pingbacks = models.IntegerField()
-    rating = models.IntegerField()
+class Player(models.Model):
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    playerfirstname = models.CharField(max_length=200)
+    playerlastname = models.CharField(max_length=200)
+    playeremail = models.CharField(max_length=200)
+    gender = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    secretary = models.BooleanField(default=False)
+    active = models.BooleanField(default=True)
+    createdate = models.DateTimeField()
+    lastupdate = models.DateTimeField()
 
     def __str__(self):
-        return self.headline
+        return self.name
+        
+class Team(models.Model):
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    teamname = models.CharField(max_length=200)
+    createdate = models.DateTimeField()
+    lastupdate = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+class Teammates(models.Model):
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    teamname = models.CharField(max_length=200)
+    createdate = models.DateTimeField()
+    lastupdate = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+class Teampairings(models.Model):
+    league = models.ForeignKey(League, on_delete=models.CASCADE)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    teamname = models.CharField(max_length=200)
+    createdate = models.DateTimeField()
+    lastupdate = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
